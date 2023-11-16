bcf2df68476b6a7a21946c64eaff42a2855be0ee
Sonika <sonikapanth@gmail.com>
2023-11-16 15:47:45 +0530

    Uploading Homepage

---
+++ client/src/Component/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div>
+    {/* <div className='flex flex-row'>
+        <div>
+          Kwizz
+        </div>
+        <div className='relative top-0 z-0'>
+         <Link className='' to={"/"}>Home</Link>
+         <Link className='' to={"/Dashboard"}>Dashboard</Link>
+         <Link className='' to={"/Contact"}>Contact</Link>
+         <Link className='' to={"/Login"}>Login</Link>
+         <Link className='' to={"/SignUp"}>SignUp</Link>
+
+        </div>
+    </div> */}
+    </div>
+  )
+}
+
+export default Navbar
+
